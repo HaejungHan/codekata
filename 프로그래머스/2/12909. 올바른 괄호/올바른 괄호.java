5fc4f3925dcf340819c691d3fcd633fda3742ef5
HaejungHan <130989670+HaejungHan@users.noreply.github.com>
2024-06-30 13:25:52 +0900

    [level 2] Title: 올바른 괄호, Time: 21.58 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,28 +1,23 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        
-        Stack<Character> stack = new Stack<>();
+        boolean answer;
+        Stack<Character> stack = new Stack<>(); 
         
         for(int i=0; i<s.length(); i++) {
-            Character c = s.charAt(i);
-            
-            if(c == '(') {
+            Character c = s.chartAt(i);
+            if (c == '(') {
                 stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
+            } else { // ')'라면
+                if (stack.isEmpty()) { // stack이 비어있는지 확인 '('없다면 올바르지 않은 괄호이기에
                     return answer = false;
-                }
-                stack.pop();
+                } 
+                stack.pop(); // '('와 짝을 맞추기 위해 push 했던 '(' 를 제거
             }
         }
-        
-        if(!stack.isEmpty()) {
+        if (!stack.isEmpty()) {
             return answer = false;
         }
-
+        
         return answer = true;
     }
 }
